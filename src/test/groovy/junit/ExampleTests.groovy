39ff768b39ed26e1f512589ceb1f271f081bb5a1
bbayhan <bulentbayhan@gmail.com>
2023-12-14 13:55:07 +0100

    2023-12-14.003

---
+++ src/test/groovy/junit/ExampleTests.groovy
@@ -0,0 +1,60 @@
+package junit
+
+import de.ser.doxis4.agentserver.AgentExecutionResult
+import ser.OutOfOffice
+import ser.OutOfOfficeSch
+import org.junit.*
+
+class ExampleTests {
+
+    Binding binding
+
+    @BeforeClass
+    static void initSessionPool() {
+        AgentTester.initSessionPool()
+    }
+
+    @Before
+    void retrieveBinding() {
+        binding = AgentTester.retrieveBinding()
+    }
+
+    @Test
+    void testForAgentResult() {
+
+        def agent = new OutOfOffice();
+
+        binding["AGENT_EVENT_OBJECT_CLIENT_ID"] = "ST0bBPM_SYS_ORG2428fed786-1795-4d88-a198-d0993a5924c6182023-12-07T08:58:24.901Z010"  //out-of-office
+
+        def result = (AgentExecutionResult)agent.execute(binding.variables)
+        System.out.println(result)
+//        assert result.resultCode == 0
+//        assert result.executionMessage.contains("Linux")
+//        assert agent.eventInfObj instanceof IDocument
+    }
+
+    @Test
+    void testForGroovyAgentMethod() {
+//        def agent = new GroovyAgent()
+//        agent.initializeGroovyBlueline(binding.variables)
+//        assert agent.getServerVersion().contains("Linux")
+    }
+
+    @Test
+    void testForJavaAgentMethod() {
+//        def agent = new JavaAgent()
+//        agent.initializeGroovyBlueline(binding.variables)
+//        assert agent.getServerVersion().contains("Linux")
+    }
+
+    @After
+    void releaseBinding() {
+        println("RLEASE BINDING RUNNING.....")
+        AgentTester.releaseBinding(binding)
+    }
+
+    @AfterClass
+    static void closeSessionPool() {
+        AgentTester.closeSessionPool()
+    }
+}
